62aed3a843ca1b5793db914e076aca41e1db9b35
Robin Stempa <stemparobin@gmail.com>
2023-01-20 09:11:13 +0100

    feat input error with errorhandling

--- src/components/login/LoginInput.js
+++ src/components/login/LoginInput.js
@@ -1,6 +1,12 @@
 import { useForm } from "react-hook-form";
 
 const LoginInput = () => {
+
+        const usernameConfig = {
+            required: true,
+            minLength: 3
+        }
+
         const {
             register,
             handleSubmit,
@@ -9,18 +15,32 @@ const LoginInput = () => {
 
         const onSubmit = (data) => {
             console.log(data)
+            console.log(data.username)
         }
-    
 
+        const errorMessage = (() => {
+            if(!errors.username) {
+                return null
+            }
+            if(errors.username.type === 'required') {
+                return <span>Username is required</span>
+            }
+
+            if(errors.username.type === 'minLength') {
+                return <span>Username is too short</span>
+            }
+        })()
+    
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input
                 type="text"
                 placeholder="Enter name"
-                {...register("name")}
+                {...register("username", usernameConfig)}
                 />
                 <button type= "submit">Start</button>
+                {errorMessage}
             </form>
         </>
     )

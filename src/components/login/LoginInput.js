c39d9cc5c85d1b41a65f435aa4c5b8dd55eeea5b
Robin Stempa <stemparobin@gmail.com>
2023-01-20 08:49:05 +0100

    feat added method for form submit

--- src/components/login/LoginInput.js
+++ src/components/login/LoginInput.js
@@ -1,13 +1,27 @@
 import { useForm } from "react-hook-form";
 
 const LoginInput = () => {
+        const {
+            register,
+            handleSubmit,
+            formState: { errors },
+        } = useForm()
+
+        const onSubmit = (data) => {
+            console.log(data)
+        }
+    
 
     return (
         <>
-        
-            <input />
-            <button>Start</button>
-        
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                type="text"
+                placeholder="Enter name"
+                {...register("name")}
+                />
+                <button type= "submit">Start</button>
+            </form>
         </>
     )
 }

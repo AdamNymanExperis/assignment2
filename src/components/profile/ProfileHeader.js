a051120f8aff1ad0927e37213b979b280bdad95c
AdamNymanExperis <122439687+AdamNymanExperis@users.noreply.github.com>
2023-01-23 13:47:00 +0100

    context profile page
    feats: display of translation history,
    navigating to translation page

---
+++ src/components/profile/ProfileHeader.js
@@ -0,0 +1,8 @@
+const ProfileHeaders = ({ username }) => {
+    return (
+        <header>
+            <h2>Hello, welcome back { username }</h2>
+        </header>
+    ) 
+}
+export default ProfileHeaders
